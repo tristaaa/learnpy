9c1023ecd6da9c57cc02aff8519c7bcbe2e49bf3
tristaaa <tristaaa56@126.com>
2018-01-31 15:04:19 +0800

    update biseciton search 1 (some fix up)

--- bisectionSearch_1.py
+++ bisectionSearch_1.py
@@ -3,24 +3,24 @@
 
 # use Bisection Search to get a close enough guess to the root of x
 # Suppose x=26
-# Bisection Search makes it quicker to find the approximate square root of x 
+# Bisection Search makes it quicker to find the approximate square root of x
 # which uses just 13 times to get the close enough square root of x
- 
+
 x = 26
 epsilon = 0.01
-low=1
-high=x
-guess=(low+high)/2
-num=0
-while abs(guess**2-x) >= epsilon:
+low = 1
+high = x
+guess = (low + high) / 2
+num = 0
+while abs(guess**2 - x) >= epsilon:
     if guess**2 < x:
         low = guess
     else:
-        high=guess
-    guess=(low+high)/2
-    num+=1
-if abs(guess**2-x)>=epsilon:
-    print('Failed on square root of',x)
+        high = guess
+    guess = (low + high) / 2
+    num += 1
+if abs(guess**2 - x) >= epsilon:
+    print('Failed on square root of', x)
 else:
-    print('num_guesses =',num)
+    print('num_guesses =', num)
     print(guess, 'is close to the square root of', x)
