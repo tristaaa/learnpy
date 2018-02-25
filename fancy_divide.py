c2a69116937dc407469e9c4f11fac73dc45231a2
tristaaa <tristaaa56@126.com>
2018-02-25 14:43:17 +0800

    return a new list, each item of the new list is the result of quotient of item divide by the list_of_numbers[index]; and when the divisor is 0, the function return and list of all 0, length keep the same

---
+++ fancy_divide.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+def fancy_divide(list_of_numbers, index):
+    denom = list_of_numbers[index]
+    return [simple_divide(item, denom) for item in list_of_numbers]
+
+
+def simple_divide(item, denom):
+    try:
+        return item / denom
+    except ZeroDivisionError:
+        return 0
+
+print(fancy_divide([0,2,4],0))
