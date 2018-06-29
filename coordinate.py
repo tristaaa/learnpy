731652d30c895d90387784d2efc0dbf0da1558a2
tristaaa <tristaaa56@126.com>
2018-06-29 16:23:56 +0800

    a class, Coordinate has x,y coordinate and some useful methods

--- coordinate.py
+++ coordinate.py
@@ -19,12 +19,23 @@ class Coordinate(object):
     def getX(self):
         return self.x
 
+    def getY(self):
+        return self.y
+
     def __str__(self):
         return "<" + str(self.x) + "," + str(self.y) + ">"
 
     def __sub__(self,other):
         return (self.x-other.x, self.y-other.y)
 
+    def __eq__(self,other):
+        # First make sure `other` is of the same type 
+        assert type(other) == type(self)
+        # Since `other` is the same type, test if coordinates are equal
+        return self.getX() == other.getX() and self.getY() == other.getY()
+
+    def __repr__(self):
+        return "Coordinate("+ str(self.getX()) +","+ str(self.getY()) +")"
 
 c = Coordinate(3,4)
 origin = Coordinate(0,0)
@@ -38,4 +49,13 @@ print(dist2) # will also show 5.0
 print(c) # will show <3,4>
 
 print(c.getX())
+
+c1 = Coordinate(3,4)
+c2 = Coordinate(4,4)
+print(c==c1)
+print(c==c2)
+
+print(repr(c)) # shows Coordinate(3,4)
+print(eval(repr(c))) # shows <3,4>
+print(eval(repr(c)) == c) # return True
 
