1502630b0fc6e472603d2d6a713f92d9e6d8633d
tristaaa <tristaaa56@126.com>
2018-08-02 14:54:24 +0800

    generate the Fabonacci number using the statement 'yield', changing the fuction genFib() to a generator, and provide the function way to get the nth fabonacci number

--- fibonacciGenerator.py
+++ fibonacciGenerator.py
@@ -21,3 +21,37 @@ print(fib.__next__()) # shows 2
 print(fib.__next__()) # shows 3
 print(fib.__next__()) # shows 5
 print(fib.__next__()) # shows 8
+
+
+#
+#-----------------------------------
+def fibonacci(n):
+    """Have to calculate all the former fibonacci number by the order""" 
+    if n < 0:
+        raise ValueError("Negative arguments not implemented")
+    return _fib(n)
+
+
+def _fib(n, fibs = [(0, 1), (1, 1)]):
+    """Returns the tuple (Fib(n), Fib(n+1))"""
+    try:
+        f = fibs[n]
+    except IndexError:
+        a, b = _fib(n // 2) # Fib(n-2), Fib(n-1)
+        c = a * (b * 2 - a)
+        d = a * a + b * b
+        if n % 2 == 0:
+            fibs.append((c, d))
+        else:
+            fibs.append((d, c + d))
+            print('in fibs:',fibs)
+        f = fibs[-1]
+    return f
+
+
+print(fibonacci(2)) 
+print(fibonacci(3))
+print(fibonacci(4))
+print(fibonacci(5))
+print(fibonacci(6))
+print(fibonacci(7))
